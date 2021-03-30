d861779b37e16da466f6352f88c211fed2c5cdfa
szezjo <szezjo@gmail.com>
2021-03-30 16:52:04 +0200

    Task5.1

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
+        // Little comment here
         System.out.println("Quaternion\nScrum Master\nszezjo\nWixaxis\nwolf4fun\nkateTsarova");
     }
 }
