6549fe3a9d8a89ebe16888ebefb124afa15d0353
szezjo <szezjo@gmail.com>
2021-04-20 16:21:38 +0200

    cleared

--- App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        // Little comment here
-        System.out.println("Quaternion\nScrum Master\nszezjo\nWixaxis\nwolf4fun\nkateTsarova");
-        // Bug comment
-    }
-}
