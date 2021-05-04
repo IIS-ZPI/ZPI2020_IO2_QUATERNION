0768d7d284685d3cce294485964329777d46b3d7
Wixaxis <wixaxis@gmail.com>
2021-05-04 16:57:29 +0200

    fully migrated

--- zpi_proj/src/test/java/com/example/AppTest.java
+++ zpi_proj/src/test/java/com/example/AppTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,21 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+    @Test
+    public void testTheTest() {
+        assertEquals(true, true);
+    }
+    
+    @Test
+    public void testTheTestTest() {
+        assertEquals(false, false);
     }
 }
