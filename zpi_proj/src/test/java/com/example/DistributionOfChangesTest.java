993e8d7d68339ca28d764a69b3864f77226f6d9c
Wixaxis <wixaxis@gmail.com>
2021-06-01 15:53:09 +0200

    initial commit

---
+++ zpi_proj/src/test/java/com/example/DistributionOfChangesTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class DistributionOfChangesTest {
+
+    
+
+}
