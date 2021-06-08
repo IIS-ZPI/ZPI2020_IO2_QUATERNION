82a6451140f50275ca72c97b20125b000e505a8b
Wixaxis <wixaxis@gmail.com>
2021-06-08 15:16:35 +0200

    fixed statistical measures tests

--- zpi_proj/src/main/java/com/example/DistributionOfChanges.java
+++ zpi_proj/src/main/java/com/example/DistributionOfChanges.java
@@ -1,17 +1,20 @@
 package com.example;
+
 import java.util.List;
 
 public class DistributionOfChanges {
+    public static final boolean readyToBeTested = false;// set to true to activate tests
     private List<Double> distribution;
 
-    DistributionOfChanges(List<Double> currency1,List<Double> currency2) {
-        if (currency1 == null || currency2 == null ||  currency1.size() != currency2.size() || currency1.size() < 2)
+    // TODO:write this
+    DistributionOfChanges(List<Double> currency1, List<Double> currency2) {
+        if (currency1 == null || currency2 == null || currency1.size() != currency2.size() || currency1.size() < 2)
             throw new IncorrectListException();
         System.out.println(currency1);
         System.out.println(currency2);
         // List<Double> diffrences = new ArrayList<>(currency1.size());
         // for(int i = 0 ;i<currency1.size();i++){
-        //     diffrences.add(currency1.get(i) - currency2.get(i));
+        // diffrences.add(currency1.get(i) - currency2.get(i));
         // }
     }
 
