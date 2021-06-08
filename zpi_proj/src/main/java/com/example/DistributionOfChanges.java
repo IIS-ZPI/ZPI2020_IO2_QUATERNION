c645c17fe5bf56605115922a6afa188230db4431
wolf4fun <wilczek4fun@gmail.com>
2021-06-08 16:18:15 +0200

    Implementing distributionOfChanges

--- zpi_proj/src/main/java/com/example/DistributionOfChanges.java
+++ zpi_proj/src/main/java/com/example/DistributionOfChanges.java
@@ -1,21 +1,28 @@
 package com.example;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DistributionOfChanges {
     public static final boolean readyToBeTested = false;// set to true to activate tests
     private List<Double> distribution;
 
-    // TODO:write this
     DistributionOfChanges(List<Double> currency1, List<Double> currency2) {
         if (currency1 == null || currency2 == null || currency1.size() != currency2.size() || currency1.size() < 2)
             throw new IncorrectListException();
-        System.out.println(currency1);
-        System.out.println(currency2);
-        // List<Double> diffrences = new ArrayList<>(currency1.size());
-        // for(int i = 0 ;i<currency1.size();i++){
-        // diffrences.add(currency1.get(i) - currency2.get(i));
-        // }
+        List<Double> dividedList= new ArrayList<>(currency1.size());
+        for(int i = 0 ;i<currency1.size();i++){
+            dividedList.add(currency1.get(i)/currency2.get(i));
+        }
+        distribution = new ArrayList<>(currency1.size());
+        distribution.add(0.0);
+        for(int i = 1 ;i<dividedList.size();i++){
+            distribution.add(dividedList.get(i)-dividedList.get(i-1));
+        }
+    }
+    @Override
+    public String toString() {
+        return distribution.toString();
     }
 
     public List<Double> getDistribution() {
